4db8dfbdbce530d7d3104ffd6504d504911c11f8
Abdallah abuFiad <a.abufiad22@gmail.com>
2023-12-10 22:59:23 +0400

    first commit

---
+++ WebApplication7/JavaScript.js
@@ -0,0 +1,30 @@
+﻿//document.write("first");
+//document.write("<br />");
+//document.write("first <br> Second <br> Third");
+//document.write("<br />");
+//console.log("ABED");
+
+//function sum(a, b)
+//{
+  //  return a + b
+//}
+
+//var total = sum(2, 5);
+
+//document.write(total);
+
+//alert("regestration done");
+//confirm("are you sure")
+
+//prompt("Please enter your Name")
+
+let a = 22;
+for (let a = 1; a < 5; a++) { document.write(a); }
+document.write("<br />");
+
+document.write(a);
+document.write("<br />");
+
+const PI = 3.141592653589793;
+var PI = 3.14;     
+document.write(pi);
